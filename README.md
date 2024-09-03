8b82fabd4a90bdc6703c9cfa2a72c9990773486a
nuuuwan <nuuuwan@gmail.com>
2024-09-03 10:36:29 +0530

    Added source, and other data

--- README.md
+++ README.md
@@ -4,25 +4,26 @@ This repository contains the manifestos of the candidates who contested the 2024
 
 ## Ranil Wickremesinghe (Independent)
 
-* [සිංහල](data/pdf-original/ind-rw-si.pdf)
-* [தமிழ்](data/pdf-original/ind-rw-ta.pdf)
+* [සිංහල (62 Pages, 11.7MB)](data/pdf-original/ind-rw-si.pdf) - [Original Source](https://www.ranil2024.lk/si/manifesto)
+* [தமிழ் (62 Pages, 5.1MB)](data/pdf-original/ind-rw-ta.pdf) - [Original Source](https://www.ranil2024.lk/ta/manifesto)
 
 ## Dilith Jayaweera (Sarvajana Balaya)
 
-* [සිංහල](data/pdf-original/mjp-si.pdf)
+* [සිංහල (66 Pages, 8.7MB)](data/pdf-original/mjp-si.pdf) - [Original Source](https://www.scribd.com/document/764382431/Presidential-Candidate-Dilith-Jayaweera-unveils-national-strategic-plan)
 
 ## Anura Kumara Dissanayake (National People's Power)
 
-* [English](data/pdf-original/npp-en.pdf)
-* [සිංහල](data/pdf-original/npp-si.pdf)
-* [தமிழ்](data/pdf-original/npp-ta.pdf)
+* [English (129 Pages, 17.5MB)](data/pdf-original/npp-en.pdf) - [Original Source](https://www.npp.lk/up/policies/en/npppolicystatement.pdf)
+* [සිංහල (233 Pages, 26.9MB)](data/pdf-original/npp-si.pdf) - [Original Source](https://www.npp.lk/up/policies/si/npppolicystatement.pdf)
+* [தமிழ் (233 Pages, 8.0MB)](data/pdf-original/npp-ta.pdf) - [Original Source](https://www.npp.lk/up/policies/ta/npppolicystatement.pdf)
 
 ## Sajith Premadasa (Samagi Jana Balawegaya)
 
-* [English](data/pdf-original/sjb-en.pdf)
-* [සිංහල](data/pdf-original/sjb-si.pdf)
-* [தமிழ்](data/pdf-original/sjb-ta.pdf)
+* [English (44 Pages, 21.0MB)](data/pdf-original/sjb-en.pdf) - [Original Source](https://cdn.prod.website-files.com/667c056ef89cca890b53adad/66d32e4698e32d285739c6ac_SajithPremadasa_policy-manifesto-english-compressed.pdf)
+* [සිංහල (38 Pages, 20.4MB)](data/pdf-original/sjb-si.pdf) - [Original Source](https://cdn.prod.website-files.com/667c056ef89cca890b53adad/66d32ef6a457cbb2fd19ac73_SajithPremadasa_policy-manifesto-sinhala-compressed.pdf)
+* [தமிழ் (60 Pages, 4.3MB)](data/pdf-original/sjb-ta.pdf) - [Original Source](https://cdn.prod.website-files.com/667c056ef89cca890b53adad/66d32e462d19d928e43d0d0b_SajithPremadasa_policy-manifesto-tamil-compressed.pdf)
 
 ## Namal Rajapakse (Sri Lanka Podujana Peramuna)
 
-* [සිංහල](data/pdf-original/slpp-si.pdf)
+* [සිංහල (50 Pages, 5.6MB)](data/pdf-original/slpp-si.pdf) - [Original Source](https://www.nr.lk/pdf/NR2024_MANIFESTO_SINHALA_PRESSQ.pdf)
+* [தமிழ் (12 Pages, 62.0MB)](data/pdf-original/slpp-ta.pdf) - [Original Source](https://www.nr.lk/pdf/NR2024_MANIFESTO_TAMIL_PRESSQ.pdf)
--- src/manifestos/Manifesto.py
+++ src/manifestos/Manifesto.py
@@ -1,6 +1,7 @@
+from functools import cached_property
 import os
 from dataclasses import dataclass
-
+from utils import JSONFile
 
 @dataclass
 class Manifesto:
@@ -8,28 +9,29 @@ class Manifesto:
 
     DIR_PDF = os.path.join('data', 'pdf-original')
     DIR_PDF_UNIX = DIR_PDF.replace('\\', '/')
+    METADATA_PATH = os.path.join('data', 'metadata.json')
 
-    @property
-    def readme_line(self):
-        return f'* [{self.lang}]({self.DIR_PDF_UNIX}/{self.file_name})'
+    @cached_property
+    def id(self):
+        return '.'.join(self.file_name.split('.')[:-1])
 
-    @property
+    @cached_property
     def lang_code(self):
-        return self.file_name.split('.')[0].split('-')[-1]
+        return self.id.split('-')[-1]
 
-    @property
+    @cached_property
     def lang(self):
         return {
             'en': 'English',
             'si': 'සිංහල',
             'ta': 'தமிழ்',
         }.get(self.lang_code, self.lang_code)
-
-    @property
+    
+    @cached_property
     def party_code(self):
-        return '-'.join(self.file_name.split('.')[0].split('-')[:-1])
+        return '-'.join(self.id.split('-')[:-1])
 
-    @property
+    @cached_property
     def party(self):
         return {
             'ind-rw': 'Ranil Wickremesinghe (Independent)',
@@ -38,7 +40,44 @@ class Manifesto:
             'sjb': 'Sajith Premadasa (Samagi Jana Balawegaya)',
             'mjp': 'Dilith Jayaweera (Sarvajana Balaya)',
         }.get(self.party_code, self.party_code)
+    
 
+
+
+    
+    @cached_property
+    def file_path(self):
+        return os.path.join(self.DIR_PDF, self.file_name)
+    
+    @cached_property 
+    def file_size(self):
+        return os.path.getsize(self.file_path)
+    
+    @cached_property
+    def n_pages(self):
+        from PyPDF2 import PdfFileReader
+        with open(self.file_path, 'rb') as f:
+            pdf = PdfFileReader(f)
+            return pdf.getNumPages()
+
+    # metadata.json
+    @staticmethod
+    def metadata_idx():
+        return JSONFile(Manifesto.METADATA_PATH).read()
+    
+    @property 
+    def source(self):
+        return self.metadata_idx()[self.id]['source']
+
+    # README
+    @cached_property 
+    def readme_line_label(self):
+        return f'{self.lang} ({self.n_pages} Pages, {self.file_size / 1_000_000:.1f}MB)'
+    @cached_property
+    def readme_line(self):
+        return f'* [{self.readme_line_label}]({self.DIR_PDF_UNIX}/{self.file_name}) - [Original Source]({self.source})'
+
+    # Loaders
     @classmethod
     def list_all(cls) -> list['Manifesto']:
         manifesto_list = []
