517a1ca21b6c2c6ba18fe41a364979a5a03eca4f
nuuuwan <nuuuwan@gmail.com>
2024-09-05 09:31:35 +0530

    dont commit README

--- workflows/readme_build.py
+++ workflows/readme_build.py
@@ -5,8 +5,6 @@ from manifestos import ReadMe
 
 def main():
     ReadMe().build()
-    os.system('git add README.md')
-    os.system('git commit -m "Updated README"')
 
 
 if __name__ == "__main__":
